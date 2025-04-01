5415bbb114e4e5020212001b9f3675a4bac6d489
Alex Ozerov <botkooper@gmail.com>
2025-04-01 03:20:37 +0300

    it works???

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,11 @@
 import Link from "next/link";
 
 export default function Page() {
-  let photos = Array.from({ length: 6 }, (_, i) => i + 1);
-
   return (
     <>
       <h1>root page</h1>
-      <section className="cards-container">
-        {photos.map((id) => (
-          <Link className="card" key={id} href={`/${id}`} passHref>
-            {id}
-          </Link>
-        ))}
-      </section>
+      Nothing to see here
+      <Link href="/photos">Go to photos</Link>
     </>
   );
 }
